5d75c7042ee7b7c99e8d77b9ed3643836ce242ba
Félix Luiz <143456284+celeron550@users.noreply.github.com>
2025-11-21 12:57:45 -0300

    tava faltando um "then" k

--- componentes/reg2.vhd
+++ componentes/reg2.vhd
@@ -14,7 +14,7 @@ architecture behav of reg2 is
 begin
     process(clk)
 begin
-    if (clk'event and clk = '1')
+    if (clk'event and clk = '1') then
         if (clr = '1') then
             q <= "00";
         elsif (ld = '1') then
